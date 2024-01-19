4a4de32f3b69171fba461fea02a35f5a6e10d81d
Gökberk Şahin <sahingokberk@gmail.com>
2024-01-19 20:10:45 +0100

    boilerplate example

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
+
+
+def main():
+    load_dotenv('.env')
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_ID = os.environ.get("CLIENT_ID")
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                           client_secret=CLIENT_SECRET))
+
+    results = sp.search(q='cramps', limit=20)
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx, track['name'])
 
 
 
 
 
-if __name__ == "main":
-    pass
+
+if __name__ == '__main__':
+    main()
+   
